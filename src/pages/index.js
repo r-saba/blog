e77e6b63ef773e706ffee05dffcfa50f729fb017
ragith <ragith.coolkid@gmail.com>
2020-05-09 21:24:37 -0400

    add redirects

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,10 +7,15 @@ import SEO from "../components/seo"
 import thinkingSVG from "../images/thinking.svg"
 import Header from "../components/header"
 import Discover from "../components/discover"
+import Helmet from "react-helmet"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Ragith" />
+    <Helmet>
+      <meta http-equiv="refresh" content="0; URL=https://www.ragith.com/blog" />
+      <link rel="canonical" href="https://www.example.com/" />
+    </Helmet>
     <Header />
     <Discover />
   </Layout>
